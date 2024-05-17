6555a60a05dde6bdf6f45d738e429c16de0516f8
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2024-05-17 20:48:27 +0530

    edit and delete features added

--- item/views.py
+++ item/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Item
-from .forms import NewItemForm
+from django.db.models import Q
+from .models import Item,Category
+from .forms import NewItemForm,EditItemForm
 from django.contrib.auth.decorators import login_required
+
+
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    query = request.GET.get('query',"")
+    category_id = request.GET.get('category',0)
+    categories = Category.objects.all()
+    
+    if category_id:
+        items = items.filter(category_id=category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    return render(request,'item/items.html',{
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id),
+    })
+
 def detail(request,pk):
     item = get_object_or_404(Item,pk=pk)
     related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
@@ -29,4 +51,31 @@ def new(request):
     return render(request,'item/form.html',{
         'form':form,
         'title':"New Item",
+    })
+    
+    
+@login_required
+def delete(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    item.delete()
+    return redirect("dashboard:index")
+
+@login_required
+def edit(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    
+    if request.method == "POST":
+        form = EditItemForm(request.POST,request.FILES,instance=item)
+        
+        if form.is_valid():
+            form.save(commit=False)
+
+            
+            return redirect("item:detail",pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+    return render(request,'item/form.html',{
+        'form':form,
+        'title':"Edit Item",
     })
